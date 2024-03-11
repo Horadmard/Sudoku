072a28f04eda51458794bbddb0be11fc73f06753
Horad <iranradmard1@gmail.com>
2024-03-11 22:07:29 +0330

    Edit

--- Node.py
+++ Node.py
@@ -11,12 +11,25 @@ class Node:
 
 class Tree:
     def __init__(self, fnode):
-        self.firstNode = fnode
+        self.firstNode = Node(fnode)
 
     def add(self, newNode):
         self.childs.append(newNode)
 
-    def printBFSTree(self):
-        print(self.element)
-        for item in self.childs:
-            printBFSTree(item)
+    # def bfs(self):
+    #     print(self.firstNode.element)
+    #     queue = self.firstNode.childs
+    #     visited = []
+
+    #     for node in queue:
+    #         if node not in visited:
+    #             print(node.)
+
+    # def dfs(self):
+    #     pass
+    
+if __name__ == "__main__":
+    node = Node(5)
+    node2 = Node(10)
+
+    node.childs.append(node2)

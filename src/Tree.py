6bd77e3baad6135f9ff38e5b898b31b8c5bd5793
Horad <iranradmard1@gmail.com>
2024-03-13 09:57:14 +0330

    small changes

--- src/Tree.py
+++ src/Tree.py
@@ -1,9 +1,10 @@
+#Implanting Node and Tree for BackTrack algorithm
+
 class Node:
     def __init__(self, value, pos=None):
         self.element = value
         self.pos = pos
         self.childs = []
-
 
 class Tree:
     def __init__(self, fnode):

58e9f3b78f9a8faa22943cedb2ef5652b6d6f090
Horad <iranradmard1@gmail.com>
2024-03-22 16:38:49 +0330

    Making progress

--- services/the_fullest_location.py
+++ services/the_fullest_location.py
@@ -1,7 +1,22 @@
-
+import numpy as np
 
 def the_fullest_location(*, sudoku: list) -> list:
 
+    # Fint the fullest block
+    m_block = sudoku[0]
+
+    for block in sudoku:
+
+        if np.count_nonzero(block == 0) < np.count_nonzero(m_block == 0):
+            m_block = block
+
+    print(m_block)
+
+    # Find the fullest row
     
+    idx = 0
+    for idx in range(2):
+
+        
 
     pass

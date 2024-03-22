d8394d892a54c00f7292d97a4b31b9e773c713b5
Horad <iranradmard1@gmail.com>
2024-03-22 16:49:30 +0330

    Making progress

--- services/the_fullest_location.py
+++ services/the_fullest_location.py
@@ -7,16 +7,20 @@ def the_fullest_location(*, sudoku: list) -> list:
 
     for block in sudoku:
 
-        if np.count_nonzero(block == 0) < np.count_nonzero(m_block == 0):
+        if np.count_nonzero(block == 0) < np.count_nonzero(m_block == 0) and np.count_nonzero(block == 0) != 0:
             m_block = block
 
     print(m_block)
 
     # Find the fullest row
     
-    idx = 0
-    for idx in range(2):
+    cnt = 0
+    for idx in range(8):
+        np.count_nonzero(sudoku[idx] == 0)
 
+
+    # Find the fullest column
         
-
+    # position = [row, col]
+    # return position
     pass

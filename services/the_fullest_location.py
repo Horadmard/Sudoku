26e53cbbab441e569b671bec297717a3d8d86fff
Horad <iranradmard1@gmail.com>
2024-03-23 02:38:42 +0330

    Add comments

--- services/the_fullest_location.py
+++ services/the_fullest_location.py
@@ -11,17 +11,31 @@ def the_fullest_location(sudoku: list) -> list:
         if row.count(0) < frow.count(0):
             frow = row
 
-    sudoku_T = [[sudoku[j][i] for j in range(len(sudoku))] for i in range(len(sudoku[0]))]
     # Find the fullest column
+    sudoku_T = [[sudoku[j][i] for j in range(len(sudoku))] for i in range(len(sudoku[0]))]
     fcol = sudoku_T[0]
 
     for col in sudoku_T:
         if col.count(0) < fcol.count(0):
             fcol = col
+    
+    # Find the fullest Block
+    
+    
+    # Define the position of most busy location
     pos = [sudoku.index(frow), sudoku_T.index(fcol)]
     return pos
 
 
 if __name__ == "__main__":
-    sudoku = [[0, 0, 0, 0, 0, 0, 0, 8, 0], [0, 6, 0, 0, 0, 4, 0, 0, 0], [0, 0, 1, 0, 0, 0, 9, 0, 0], [2, 1, 0, 0, 8, 0, 0, 0, 0], [0, 0, 0, 0, 1, 7, 0, 0, 6], [0, 0, 3, 0, 0, 0, 4, 0, 0], [0, 0, 0, 0, 9, 0, 0, 0, 0], [7, 0, 8, 0, 0, 0, 0, 2, 0], [4, 0, 9, 6, 0, 0, 7, 0, 0]]
+    sudoku =[[0, 0, 0, 0, 0, 0, 0, 8, 0],
+             [0, 6, 0, 0, 0, 4, 0, 0, 0],
+             [0, 0, 1, 0, 0, 0, 9, 0, 0],
+             [2, 1, 0, 0, 8, 0, 0, 0, 0],
+             [0, 0, 0, 0, 1, 7, 0, 0, 6],
+             [0, 0, 3, 0, 0, 0, 4, 0, 0],
+             [0, 0, 0, 0, 9, 0, 0, 0, 0],
+             [7, 0, 8, 0, 0, 0, 0, 2, 0],
+             [4, 0, 9, 6, 0, 0, 7, 0, 0]]
+    
     print(the_fullest_location(sudoku))

c736f2a5aad3ccf928992059914f22d3084053b4
Horad <iranradmard1@gmail.com>
2024-03-23 22:43:35 +0330

    Clean code

--- services/data.py
+++ services/data.py
@@ -6,13 +6,16 @@ def importData():
     df = pn.read_csv('../docs/sample.csv')
 
 
-    col1 = df['Puzzle'][rand.randint(0, 19)]
-    col1 = col1.replace('.', '0')
+    random_col = df['Puzzle'][rand.randint(0, 19)]
+    random_col = random_col.replace('.', '0')
 
-    numbers = [int(x) for x in col1]
+
+    numbers = [int(x) for x in random_col]
     matrix = [numbers[i * 9:(i + 1) * 9] for i in range(9)]
+
 
     # for row in matrix:
     #     print(row)
     
     return matrix
+

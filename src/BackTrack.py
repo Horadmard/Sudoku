982394065fb3d73dd12e50a044825acbbd6361b7
Horad <iranradmard1@gmail.com>
2024-03-28 01:27:59 +0330

    init comment

--- src/BackTrack.py
+++ src/BackTrack.py
@@ -1,20 +1,47 @@
-# The algorithm is not working to be fixed
+import sys
+sys.path.insert(0, '../services/')  # Replace with the actual path
 
-def dfs(self, visited=None):
-    if visited is None:
-        visited = set()
+from data import importData
+from the_fullest_location import the_fullest_location
+from update import update_with_value, update
 
-    stack = []
-    stack.append()
-    stack.pop()
 
-    order = []
+stack = []
+# stack.append()
+# stack.pop()
+path = []
 
-    if self.firstNode not in visited:
-        order.append(self.firstNode)
-        visited.add(self.firstNode)
-    for node in self[self.firstNode]:
-        if node not in visited:
-            order.extend(dfs(self, visited))
 
-    return order
+def solve_sudoku(*, sudoku: list) -> list:
+
+    # defin node
+    loc, values = the_fullest_location(sudoku=sudoku)
+
+    # if there is no choice return to previous state
+    if values == None: 
+        stack.pop()
+        return 0
+
+
+    stack.append(values)
+    if stack.count() == max:
+        pass
+
+    # solve the rest of sudoku
+    update_with_value(sudoku=sudoku, location=loc, value=stack.pop())
+
+    solve_sudoku(sudoku=sudoku)
+
+    pass
+
+
+if __name__ == "__main__":
+
+    List = [1, 2, 3, 4, 5]
+    List.append(1)
+    List.pop()
+    List.pop()
+    List.pop()
+
+    print(List)
+    solve_sudoku(sudoku=importData())

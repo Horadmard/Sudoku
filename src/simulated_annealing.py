4b55ed960f2abbf1587dff519c47dadf2b62fa28
Horad <iranradmard1@gmail.com>
2024-06-06 22:39:53 +0330

    in progress

--- src/simulated_annealing.py
+++ src/simulated_annealing.py
@@ -1,7 +1,19 @@
 
+from data import importData
 
-def solve(inital_temp, crate, org_sudoku, iteration):
+def solve(inital_temp, crate, org_sudoku, iter):
 
     # code 
-    
+
     pass
+
+
+if __name__ == "__main__":
+
+    inital_temp = 1000
+    cooling_rate = .99
+    iteration = 100
+
+    sudoku = importData()
+
+    solve(inital_temp=inital_temp, crate=cooling_rate, iter=iteration, org_sudoku=sudoku)

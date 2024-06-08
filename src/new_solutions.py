25c8f7170820f332b90f9dc6e488a799320fb35a
Horad <iranradmard1@gmail.com>
2024-06-08 04:04:07 +0330

    debug

--- src/new_solutions.py
+++ src/new_solutions.py
@@ -11,26 +11,33 @@ def new_solution_maker(*, org_sudoku: list, cur_sudoku: list) -> list:
 
     element_row, element_col = rand.randint(0, 2), rand.randint(0, 2)
     element_row_new, element_col_new = rand.randint(0, 2), rand.randint(0, 2)
+    print(block_row, block_col)
+    print(element_row, '-',element_col)
+    print(element_row_new, '-',element_col_new)
+
+
+    if element_row == element_row_new and element_col == element_col_new:
+        return new_solution_maker(org_sudoku=org_sudoku, cur_sudoku=cur_sudoku)
+    
 
     actual_row = block_row * 3 + element_row
     actual_col = block_col * 3 + element_col
 
     actual_row_new = block_row * 3 + element_row_new
     actual_col_new = block_col * 3 + element_col_new
+    
+    if org_sudoku[actual_row][actual_col] != 0 or org_sudoku[actual_row_new][actual_col_new] != 0:
+        return new_solution_maker(org_sudoku=org_sudoku, cur_sudoku=cur_sudoku)
+    
+    a = cur_sudoku[actual_row_new][actual_col_new]
+    b = cur_sudoku[actual_row][actual_col]
+    
+    new_sudoku[actual_row][actual_col] = a
+    new_sudoku[actual_row_new][actual_col_new] = b
+    
 
-    while org_sudoku[actual_row][actual_col] == 0 and org_sudoku[actual_row_new][actual_col_new] == 0:
-        block_row, block_col = rand.randint(0, 2), rand.randint(0, 2)
-
-        element_row, element_col = rand.randint(0, 2), rand.randint(0, 2)
-        element_row_new, element_col_new = rand.randint(0, 2), rand.randint(0, 2)
-
-        actual_row = block_row * 3 + element_row
-        actual_col = block_col * 3 + element_col
-
-        actual_row_new = block_row_new * 3 + element_row_new
-        actual_col_new = block_col_new * 3 + element_col_new
-
-    new_sudoku[actual_row][actual_col] = cur_sudoku[actual_row_new][actual_col_new]
-    new_sudoku[actual_row_new][actual_col_new] = cur_sudoku[actual_row][actual_col]
-
+    
     return new_sudoku
+
+
+
--- src/test.py
+++ src/test.py
@@ -6,16 +6,54 @@ from new_solutions import new_solution_maker
 
 
 
-sudoku = importData()
-cur_sudoku = initial_solution(org_sudoku=sudoku)
+# sudoku = importData()
+# cur_sudoku = initial_solution(org_sudoku=sudoku)
 
-for row in cur_sudoku:
-    print(row)
+# for row in cur_sudoku:
+#     print(row)
 
-print('---------------------------------')
+# print('---------------------------------')
 
-cur_sudoku = new_solution_maker(org_sudoku=sudoku, cur_sudoku=cur_sudoku)
+# cur_sudoku = new_solution_maker(org_sudoku=sudoku, cur_sudoku=cur_sudoku)
 
 
-for row in cur_sudoku:
+# for row in cur_sudoku:
+#     print(row)
+
+
+
+
+matrix = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+cur = [
+    [5, 3, 1, 8, 7, 3, 1, 4, 9],
+    [6, 4, 7, 1, 9, 5, 2, 8, 7],
+    [2, 9, 8, 4, 2, 6, 3, 6, 5],
+    [8, 3, 1, 1, 6, 4, 8, 4, 3],
+    [4, 9, 2, 8, 5, 3, 2, 9, 1],
+    [7, 6, 5, 9, 2, 7, 5, 7, 6],
+    [4, 6, 8, 5, 7, 6, 2, 8, 1],
+    [2, 5, 3, 4, 1, 9, 4, 6, 5],
+    [7, 9, 1, 3, 8, 2, 3, 7, 9]
+
+]
+
+
+# cur = initial_solution(org_sudoku=matrix)
+
+
+
+mat = new_solution_maker(org_sudoku=matrix, cur_sudoku=cur)
+
+for row in mat:
     print(row)
